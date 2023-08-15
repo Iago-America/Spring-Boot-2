9464354023615f38284d8ea2ae4498c560f8c9c3
Iago América <90301260+Iago-America@users.noreply.github.com>
2023-08-16 00:02:13 +0100

    Spring Data JPA pt 01

--- src/main/java/academy/devdojo/springboot2/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -2,10 +2,20 @@ package academy.devdojo.springboot2.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data //Domain representa basicamente o que vem do banco de dados
 @AllArgsConstructor //Gera um construtor com todos o valores
+@NoArgsConstructor
+@Entity
 public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
