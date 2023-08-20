1e9fcda1006bca1799159ff22926be72b0dc0d20
Iago América <90301260+Iago-America@users.noreply.github.com>
2023-08-20 19:13:49 +0100

    Spring Data JPA pt 02

--- src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
+++ src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
@@ -1,9 +1,10 @@
 package academy.devdojo.springboot2.repository;
 
 import academy.devdojo.springboot2.domain.Anime;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 //Repository basicamente a conexão com o banco de dados )Listar, procurar etc)
-public interface AnimeRepository {
-    List<Anime> listAll();
+public interface AnimeRepository extends JpaRepository<Anime, Long> {
+
 }
